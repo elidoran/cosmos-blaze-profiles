380cb8e24ee6aed5f3b80fbc832174b50a712368
Eli Doran <eli@elidoran.com>
2015-12-29 06:27:58 -0500

    =0.1.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cosmos:blaze-profiles',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'Reusable Blaze Templates via function profiles',
   git: 'https://github.com/elidoran/cosmos-blaze-profiles',
   documentation: 'README.md'
